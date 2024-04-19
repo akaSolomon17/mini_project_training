1634dba10012200e0c9f7bb4141d9038a9a12698
mafiavietquat <quochuy.4work@gmail.com>
2024-04-19 10:08:17 +0700

    list function to do on main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+/**
+ * Chức năng cần thực hiện:
+ * 1. Play/ Pause.
+ * 2. Xoay đĩa khi play & Dừng đĩa khi pause.
+ * 3. Next / Previous / Autoplay function.
+ * 4. Random/Repeat/Normal sẽ dùng chung một nút để chuyển đổi.
+ * 5. Highlight bài đáng phát trong playlist.
+ * 6. Tua nhạc trên thanh progress và hiển thị thời gian phát nhạc.
+ * 7. Volume tăng giảm hoặc tắt âm lượng.
+ * 8. Thêm bài nhạc vào dsach yêu thích bằng nút tim, lưu trạng thái yêu thích khi tải lại trang.
+ * 9. Thêm 1 banner quảng cáo dưới mỗi 3 bài nhạc trong playlist.
+ */
+
